f83b12bd75ec5d5793eebbe694ec49da1415ef94
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-07-11 09:51:13 -0500

    you are not your fucking khakis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectMayhem::Application.config.secret_token = 'ffb8fb101bbc2ac189711ccc45e357bb67eec7708de5d4cb8cebb2c0d54036bed3f2aa8ce9fccff849d046fb5c28916da3697e6fb0e519803454f66e4836b81e'
