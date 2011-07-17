c1651d45f678a65a1d3d46a2da30e76ccd7987a4
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-07-17 12:05:40 -0500

    got the user model and action mailer working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,25 @@
 ProjectMayhem::Application.configure do
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'neal@codeacademy.org',
+    :password             => 'codeacademy',
+    :authentication       => 'plain',
+    }
+  
+  
+  # config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   :location => '/usr/sbin/sendmail',
+  #   :arguments => '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  
+  
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
@@ -15,7 +36,7 @@ ProjectMayhem::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
