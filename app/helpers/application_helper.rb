44b4d99a3f18734f1f3545c4ac36382ff89b480b
Justin Love <git@JustinLove.name>
2011-08-02 14:44:47 -0500

    highlight current nav section

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_link_to(title, path)
+    if (current_page?(path))
+      link_to title, path, :class => 'active'
+    else
+      link_to title, path
+    end
+  end
 end
