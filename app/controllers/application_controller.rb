01cd50f20d65a05671d95f6f94aa2eccc2f880ee
Jeff Cohen <jeff@purpleworkshops.com>
2011-08-06 14:16:34 -0500

    Start mobile site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  
+  before_filter :detect_mobile_device
+
   protect_from_forgery
+  
+  def detect_mobile_device
+    request.format = :mobile if request.user_agent =~ /Mobile/
+  end
+  
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
+  
+  def home
+    respond_to do |format|
+      format.html
+      format.mobile
+    end
+  end
+  
   def lightbox
     render :layout => nil
   end
+  
+  
 end
