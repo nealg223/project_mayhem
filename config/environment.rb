686ba2d74982edcc39a57fbe2d125b70a09a2610
Mike McGee <michael.d.mcgee@gmail.com>
2011-08-08 20:11:16 -0500

    removed config.gem from the environment.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ProjectMayhem::Application.initialize!
-
-# Adds a tweet button to the rails application
-config.gem "tweet-button"
