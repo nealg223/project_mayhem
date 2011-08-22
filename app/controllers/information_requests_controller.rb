aed544b34e21b7e4d23733e9b6716000c2ca299f
Jeff Casimir <jeff@casimircreative.com>
2011-08-21 22:07:44 -0400

    Cutting down respond_to by using the class method

--- app/controllers/information_requests_controller.rb
+++ app/controllers/information_requests_controller.rb
@@ -1,36 +1,21 @@
 class InformationRequestsController < ApplicationController
+  respond_to :html
 
   def thanks
     @email = params[:email]
-
-    respond_to do |format|
-      format.html # show.html.erb
-    end
   end
-
 
   def new
     @information_request = InformationRequest.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-    end
   end
-
 
   def create
     @information_request = InformationRequest.new(params[:information_request])
-
-    respond_to do |format|
-      if @information_request.save
-        InformationRequestMailer.welcome_email(@information_request).deliver
-        
-        format.html { redirect_to(:controller => :information_requests, :action => :thanks, :email => @information_request.email) }
-      
-      else
-        format.html { render :action => "new" }
-      end
+    if @information_request.save
+      InformationRequestMailer.welcome_email(@information_request).deliver
+      redirect_to thanks_information_requests(:email => @information_request.email)
+    else
+      render :action => "new"
     end
   end
-
 end
