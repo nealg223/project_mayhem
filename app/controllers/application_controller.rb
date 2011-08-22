bae8697cc2b0bd8ca915af8ef4c6cf33564b164d
Jeff Casimir <jeff@casimircreative.com>
2011-08-21 21:58:41 -0400

    Whitespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,9 @@
 class ApplicationController < ActionController::Base
-
-  
   before_filter :detect_mobile_device
-
   protect_from_forgery
-  
+
+private  
   def detect_mobile_device
     request.format = :mobile if request.user_agent =~ /Mobile|Android/
-  end
-  
+  end  
 end
