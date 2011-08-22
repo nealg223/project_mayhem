07913844806d6291a167f477cdabc967333b9408
Jeff Casimir <jeff@casimircreative.com>
2011-08-21 22:11:02 -0400

    Ternary for less code, less repetition

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,6 @@
 module ApplicationHelper
   def active_link_to(title, path)
-    if (current_page?(path))
-      link_to title, path, :class => 'active'
-    else
-      link_to title, path
-    end
+    link_to title, path, 
+      :class => (current_page?(path) ? 'active' : '')
   end
 end
